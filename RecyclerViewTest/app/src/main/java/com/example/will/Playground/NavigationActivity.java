cef14c163a38dd9d96cb9e235d90583cb52535cb
willjgriff <willjgriff@gmail.com>
2016-02-21 22:10:43 +0000

    Completed basic navigation activity. Now to use a RecyclerView instead of a ListView...

--- RecyclerViewTest/app/src/main/java/com/example/will/Playground/NavigationActivity.java
+++ RecyclerViewTest/app/src/main/java/com/example/will/Playground/NavigationActivity.java
@@ -2,9 +2,14 @@ package com.example.will.Playground;
 
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +20,7 @@ import java.util.List;
 public class NavigationActivity extends AppCompatActivity {
 
     private ListView mNavigationList;
+    private ActionBarDrawerToggle mNavigationToggle;
     private DrawerLayout mNavigationDrawer;
     private String mActivityTitle;
 
@@ -29,6 +35,37 @@ public class NavigationActivity extends AppCompatActivity {
 
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, getNavigationItems());
         mNavigationList.setAdapter(arrayAdapter);
+        mNavigationList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(NavigationActivity.this, "YOLO: " + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        setNavigationToggle();
+
+    }
+
+    private void setNavigationToggle() {
+        mNavigationToggle = new ActionBarDrawerToggle(this, mNavigationDrawer, R.string.drawer_open, R.string.drawer_close) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                // We can do stuff here
+//                invalidateOptionsMenu();
+            }
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                // We can do stuff here
+//                invalidateOptionsMenu();
+            }
+        };
+        mNavigationToggle.setDrawerIndicatorEnabled(true);
+        mNavigationDrawer.setDrawerListener(mNavigationToggle);
     }
 
     private List<String> getNavigationItems() {
@@ -37,5 +74,16 @@ public class NavigationActivity extends AppCompatActivity {
         navigationItems.add("Dua");
         navigationItems.add("Tiga");
         return navigationItems;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item) || mNavigationToggle.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mNavigationToggle.syncState();
     }
 }
