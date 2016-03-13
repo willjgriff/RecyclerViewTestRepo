4df0fe1f77faa1d02f7c237b798b5bd1e3efefd2
willjgriff <willjgriff@gmail.com>
2016-03-13 00:32:50 +0000

    Added a fab screen. Fab disappears on scroll. Altered appearance of tabLayout. Added base structure for WebView.

--- playground/navigation/src/main/java/navigation/NavigationActivity.java
+++ playground/navigation/src/main/java/navigation/NavigationActivity.java
@@ -16,15 +16,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 
-import com.example.will.Playground.OtherFragment;
 import com.example.will.Playground.R;
 import com.example.will.Playground.Utils.UiUtils;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import fab.FabFragment;
 import lists.ListsFragment;
 import tabs.TabLayoutFragment;
+import web.WebViewFragment;
 
 /**
  * Created by Will on 18/02/2016.
@@ -38,8 +39,9 @@ public class NavigationActivity extends AppCompatActivity {
 
     enum Tag {
         LISTS(R.string.action_list_activity),
-        OTHER(R.string.action_other_fragment),
-        TABS(R.string.action_tab_layout_fragment);
+        FAB(R.string.action_fab_fragment),
+        TABS(R.string.action_tab_layout_fragment),
+        WEB(R.string.action_web_view_fragment);
 
         int mTitle;
 
@@ -100,21 +102,7 @@ public class NavigationActivity extends AppCompatActivity {
     }
 
     private void setNavigationToggle() {
-        mNavigationToggle = new ActionBarDrawerToggle(this, mNavigationDrawer, R.string.drawer_open, R.string.drawer_close) {
-            @Override
-            public void onDrawerOpened(View drawerView) {
-                super.onDrawerOpened(drawerView);
-                // We can do stuff here
-//                invalidateOptionsMenu();
-            }
-
-            @Override
-            public void onDrawerClosed(View drawerView) {
-                super.onDrawerClosed(drawerView);
-                // We can do stuff here
-//                invalidateOptionsMenu();
-            }
-        };
+        mNavigationToggle = new ActionBarDrawerToggle(this, mNavigationDrawer, R.string.drawer_open, R.string.drawer_close);
         mNavigationToggle.setDrawerIndicatorEnabled(true);
         mNavigationDrawer.setDrawerListener(mNavigationToggle);
     }
@@ -125,7 +113,7 @@ public class NavigationActivity extends AppCompatActivity {
 
         FragmentTransaction fragmentTransaction = fragmentManager
                 .beginTransaction()
-                .setCustomAnimations(R.anim.fragment_fade_in, R.anim.fragment_fade_out)
+                .setCustomAnimations(R.anim.fragment_fade_in, R.anim.fragment_fade_out, R.anim.fragment_fade_in, R.anim.fragment_fade_out)
                 .replace(R.id.activity_navigation_fragment_holder, fragment, tag.name());
 
         if (!currentFragment.getTag().equals(tag.name())) {
@@ -134,6 +122,9 @@ public class NavigationActivity extends AppCompatActivity {
 
         fragmentTransaction.commit();
         getSupportActionBar().setTitle(title);
+        // Unfortunate sometimes unnecessary command as tabLayout must remove the elevation.
+        getSupportActionBar().setElevation(UiUtils.convertDpToPixel(4, this));
+
         mNavigationDrawer.closeDrawer(mNavigationList);
     }
 
@@ -144,16 +135,22 @@ public class NavigationActivity extends AppCompatActivity {
                 replaceFragment(new ListsFragment(), getString(Tag.LISTS.getTitle()), Tag.LISTS);
             }
         }));
-        mNavEntries.add(new NavigationEntry(getString(Tag.OTHER.getTitle()), new View.OnClickListener() {
+        mNavEntries.add(new NavigationEntry(getString(Tag.FAB.getTitle()), new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                replaceFragment(new OtherFragment(), getString(Tag.OTHER.getTitle()), Tag.OTHER);
+                replaceFragment(new FabFragment(), getString(Tag.FAB.getTitle()), Tag.FAB);
             }
         }));
         mNavEntries.add(new NavigationEntry(getString(Tag.TABS.getTitle()), new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 replaceFragment(new TabLayoutFragment(), getString(Tag.TABS.getTitle()), Tag.TABS);
+            }
+        }));
+        mNavEntries.add(new NavigationEntry(getString(Tag.WEB.getTitle()), new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replaceFragment(new WebViewFragment(), getString(Tag.WEB.getTitle()), Tag.WEB);
             }
         }));
     }
---
+++ playground/web/src/main/java/web/WebViewFragment.java
@@ -0,0 +1,23 @@
+package web;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.will.Playground.R;
+
+/**
+ * Created by Will on 13/03/2016.
+ */
+public class WebViewFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_web_view, container, false);
+        return view;
+    }
+}
