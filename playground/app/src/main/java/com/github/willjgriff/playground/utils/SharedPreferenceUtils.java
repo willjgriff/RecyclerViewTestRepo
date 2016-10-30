eeefb05f25f4c2eb221e67238ad4468df418d35b
willjgriff <willjgriff@gmail.com>
2016-10-30 19:52:39 +0000

    Updated SharedPreferenceUtis. Also updated targetSdk and other things incl support lib.

--- playground/app/src/main/java/com/github/willjgriff/playground/utils/SharedPreferenceUtils.java
+++ playground/app/src/main/java/com/github/willjgriff/playground/utils/SharedPreferenceUtils.java
@@ -6,6 +6,7 @@ import android.content.SharedPreferences.Editor;
 import android.preference.PreferenceManager;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 /**
  * Created by Will on 03/04/2016.
@@ -16,19 +17,23 @@ public class SharedPreferenceUtils {
 
     public static final String SHARED_MOVIES_CONFIG = "com.github.willjgriff.playground.utils;SHARED_MOVIES_CONFIG";
 
-    public static void writeObjectToPreferences(Context context, String key, Object value) {
+    public static <T> void writeObjectToPreferences(Context context, String key, T value) {
         Gson gson = new Gson();
-        SharedPreferences prefs = context.getSharedPreferences(SHARED_PREFS_NAME, Context.MODE_PRIVATE);
-        Editor prefsEditor = prefs.edit();
         String json = gson.toJson(value);
-        prefsEditor.putString(key, json).apply();
+        SharedPreferences sharedPreferences = context.getSharedPreferences(SHARED_PREFS_NAME, Context.MODE_PRIVATE);
+        sharedPreferences.edit().putString(key, json).apply();
     }
 
     public static <T> T readObjectFromPreferences(Context context, String key, Class<T> returnType) {
-        PreferenceManager.getDefaultSharedPreferences(context);
         Gson gson = new Gson();
-        SharedPreferences prefs = context.getSharedPreferences(SHARED_PREFS_NAME, Context.MODE_PRIVATE);
-        return gson.fromJson(prefs.getString(key, ""), returnType);
+        SharedPreferences sharedPreferences = context.getSharedPreferences(SHARED_PREFS_NAME, Context.MODE_PRIVATE);
+        return gson.fromJson(sharedPreferences.getString(key, ""), returnType);
+    }
+
+    public static <T> T readComplexObjectFromPreferences(Context context, String key, TypeToken<T> typeToken) {
+        Gson gson = new Gson();
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        return gson.fromJson(sharedPreferences.getString(key, ""), typeToken.getType());
     }
 
 }
